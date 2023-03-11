388f99a6d2137293f12f287c9f4da3c13cebe60b
Hanna <anna.kozyreva.w@gmail.com>
2023-03-11 13:57:42 +0300

    fixture and new check added

--- tests/Student_Registration_Form.py
+++ tests/Student_Registration_Form.py
@@ -1,41 +1,49 @@
-from selene import browser, have
+import pytest
+from selene import browser, have, be
+from selene.support.shared import browser
 
-#browser.config.browser_name = 'firefox'
-browser.config.hold_browser_open = True
+def test_form(browser_size_w1920_h1080):
+    browser.open('https://demoqa.com/automation-practice-form')
+    browser.element('#firstName').type('Иван')
+    browser.element('#lastName').type('Иванов')
+    browser.element('#userEmail').type('qa_test@test.ru')
+    browser.element('[for="gender-radio-1"]').click()
+    browser.element('#userNumber').type('3334445555')
 
-browser.open('https://demoqa.com/automation-practice-form')
-# browser.element('#firstName').type('Иван')
-# browser.element('#lastName').type('Иванов')
-# browser.element('#userEmail').type('qa_test@test.ru')
-# browser.element('[for="gender-radio-1"]').click()
-# browser.element('#userNumber').type('3334445555')
-#
-# # Choose Birthdate
-# browser.element('#dateOfBirthInput').click()
-# browser.element('.react-datepicker__month-select').click()
-# browser.element('option[value="7"]').click()
-# browser.element('.react-datepicker__year-select').click()
-# browser.element('option[value="2001"]').click()
-# browser.element('[class="react-datepicker__day react-datepicker__day--029"]').click()
-#
-# browser.element('#subjectsInput').type('Comm').element('//div[contains(text(),"Commerce")]').click()
-# browser.element('[for=hobbies-checkbox-3]').click()
-# browser.element('#uploadPicture').send_keys('C:/Users/Hanna/Downloads/Без названия.jpg')
+    # Choose Birthdate
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('option[value="7"]').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('option[value="2001"]').click()
+    browser.element('[class="react-datepicker__day react-datepicker__day--029"]').click()
 
-# browser.element('#currentAddress').type("Планета Земля")
-browser.element('[id="state"] [class=" css-1wy0on6"] div').click()
-#browser.element('#react-select-3-option-2').click()
-browser.element('[id="submit"]').submit()
+    browser.element('#subjectsInput').type('Comm').element('//div[contains(text(),"Commerce")]').click()
+    browser.element('[for=hobbies-checkbox-3]').click()
+    browser.element('#uploadPicture').send_keys('C:/Users/Hanna/Downloads/Без названия.jpg')
 
-# Inputs control
-browser.element('#example-modal-sizes-title-lg').should(have.text("Thanks"))
-browser.element('//tbody/tr[1]/td[2]').should(have.text("Иван Иванов"))
-browser.element('//tbody/tr[2]/td[2]').should(have.text("qa_test@test.ru"))
-browser.element('//tbody/tr[3]/td[2]').should(have.text("Male"))
-browser.element('//tbody/tr[4]/td[2]').should(have.text("3334445555"))
-browser.element('//tbody/tr[5]/td[2]').should(have.text("29 August,2001"))
-browser.element('//tbody/tr[6]/td[2]').should(have.text("Commerce"))
-browser.element('//tbody/tr[7]/td[2]').should(have.text("Music"))
-browser.element('//tbody/tr[9]/td[2]').should(have.text("Планета Земля"))
+    browser.element('#currentAddress').type("Планета Земля")
+    browser.element('#react-select-3-input').type('ra')
+    browser.element('#react-select-3-option-1').click()
+    browser.element('#react-select-4-input').type(' ')
+    browser.element('#react-select-4-option-1').click()
 
-#browser.element('[id="closeLargeModal"]').click()
+    browser.element('[id="submit"]').submit()
+
+    # Inputs control
+    browser.element('#example-modal-sizes-title-lg').should(have.text("Thanks"))
+    browser.element('//tbody/tr[1]/td[2]').should(have.text("Иван Иванов"))
+    browser.element('//tbody/tr[2]/td[2]').should(have.text("qa_test@test.ru"))
+    browser.element('//tbody/tr[3]/td[2]').should(have.text("Male"))
+    browser.element('//tbody/tr[4]/td[2]').should(have.text("3334445555"))
+    browser.element('//tbody/tr[5]/td[2]').should(have.text("29 August,2001"))
+    browser.element('//tbody/tr[6]/td[2]').should(have.text("Commerce"))
+    browser.element('//tbody/tr[7]/td[2]').should(have.text("Music"))
+    browser.element('//tbody/tr[9]/td[2]').should(have.text("Планета Земля"))
+
+    browser.element('[id="closeLargeModal"]').click()
+    # Empty fields check
+    browser.element('#firstName').should(be.blank)
+    browser.element('#lastName').should(be.blank)
+    browser.element('#userEmail').should(be.blank)
+    browser.element('#userNumber').should(be.blank)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,17 @@
 import pytest
 from selene import browser
-
+from selene.support.shared import browser
 
 @pytest.fixture()
-def browser_manage ()
-    browser.config.browser_name = "chrome"
+def browser_size_w1920_h1080():
+    #browser.config.browser_name = "Firefox"
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    yield browser_size_w1920_h1080
+    browser.config.hold_browser_open = True
+
+@pytest.fixture()
+def browser_size_w1280_h720():
+    #browser.config.browser_name = "Firefox"
+    browser.config.window_width = 1280
+    browser.config.window_height = 720
